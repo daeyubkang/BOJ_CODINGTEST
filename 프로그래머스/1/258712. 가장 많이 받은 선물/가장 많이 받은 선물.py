9feb09e9497ca46a093d175ff43e0d640f44fc5b
daeyubkang <121339830+daeyubkang@users.noreply.github.com>
2024-01-22 16:59:49 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.61 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,57 @@
+def solution(friends, gifts):
+    answer = 0
+    names = {}
+    for i,val in enumerate(friends):
+        names[val] = {"get": {}, "put": {}, "num": 0, "gifts": {}}
+
+    
+
+    for i in gifts:
+        p1 = i.split(" ")[0]
+        p2 = i.split(" ")[1]
+        if (names[p1]["put"].get(p2)):
+            names[p1]["put"][p2] += 1
+        else:
+            names[p1]["put"][p2] = 1
+
+        if (names[p2]["get"].get(p1)):
+            names[p2]["get"][p1] += 1
+        else:
+            names[p2]["get"][p1] = 1
+        names[p1]["num"] += 1
+        names[p2]["num"] -= 1
+
+        if (names[p1]["gifts"].get(p2)):
+            names[p1]["gifts"][p2] += 1
+        else:
+            names[p1]["gifts"][p2] = 1
+
+        if (names[p2]["gifts"].get(p1)):
+            names[p2]["gifts"][p1] -= 1
+        else:
+            names[p2]["gifts"][p1] = -1
+    result = []
+    for i, name in enumerate(friends):
+        giftnum = 0
+        for name2 in friends:
+            if name == name2:
+                continue
+            else:
+                if(names[name]["gifts"].get(name2)):
+                    if (names[name]["gifts"].get(name2) > 0):
+                        giftnum += 1
+                        continue
+                    elif (names[name]["gifts"].get(name2) < 0):
+                        continue
+                if(names[name]["num"] > names[name2]["num"]):
+                    giftnum += 1
+        result.append(giftnum)
+
+    
+
+    print(result)
+    
+        
+    answer = max(result)
+    
+    return answer
