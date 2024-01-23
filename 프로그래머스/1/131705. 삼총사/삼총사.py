d4233f7048e9c058178f435aa055193ec5e766a9
daeyubkang <121339830+daeyubkang@users.noreply.github.com>
2024-01-23 18:19:47 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,13 @@
+def solution(number):
+    answer = 0
+    dic1 = {}
+    number.sort()
+    for i in number:
+        dic1[i] = 1
+    for i in range(len(number)):
+        if number[i] > 0: break
+        for j in range(i+1,len(number)):
+            for k in range(j+1,len(number)):
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+    return answer
