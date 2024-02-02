990e0e2a068387208c3e8d483db465cd681ad0b4
daeyubkang <121339830+daeyubkang@users.noreply.github.com>
2024-02-02 22:37:36 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -3,20 +3,13 @@ def solution(number, k):
     arr1 = [int(i) for i in number]
     arr2 = []
     for i in arr1:
-        flag = False
-        if not arr2 or k==0:
-            arr2.append(i)
-            continue
-        while arr2:
+        while arr2 and k:
             if arr2[-1] < i and k:
                 arr2.pop()
                 k -= 1
             else:
-                arr2.append(i)
-                flag = True
                 break
-        if not flag:
-            arr2.append(i)
+        arr2.append(i)
     for i in range(k):
         arr2.pop()
     for i in arr2:
